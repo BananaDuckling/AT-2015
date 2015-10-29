2dd11897b31ef668f622e1cb791a6d7456b2bd7e
16jc0080 <16jc0080@HSS-101-10.westwindsor.k12.nj.us>
2015-10-29 13:41:31 -0400

    errors maybe? lol

--- LinkedLists/src/linkedLists/AlgebraCalc.java
+++ LinkedLists/src/linkedLists/AlgebraCalc.java
@@ -1,12 +1,35 @@
 package linkedLists;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class AlgebraCalc {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Polynomial s = new Polynomial();
+        s.add(new Term(3, 5));
+        s.add(new Term(2, 4));
+        s.add(new Term(4, 4));
+        System.out.println("s: " + s);
+        
+        Polynomial q = new Polynomial();
+        q.add(new Term(2, 2));
+        q.add(new Term(1, 2));
+        System.out.println("q: " + q);
+        
+        s.multiply(q);
+        System.out.println(s.toString());
+        
+        
+        s = new Polynomial();
+        s.add(new Term(3, 5));
+        s.add(new Term(2, 4));
+        s.add(new Term(4, 4));
+        s.add(q);
+        
+        System.out.println(s.derivative());
 
-	}
-	
-	public 
+        System.out.println(s.toString());
+    }
 
 }
--- LinkedLists/src/linkedLists/Polynomial.java
+++ LinkedLists/src/linkedLists/Polynomial.java
@@ -1,16 +1,159 @@
 package linkedLists;
+
+import java.util.AbstractList;
 import java.util.LinkedList;
 import java.util.ListIterator;
+
 public class Polynomial {
-	private LinkedList<TermStore> list;
-	
-	public Polynomial(LinkedList<TermStore> l)
-	{
-		list = l;
-	}
-	
-	public void addTerm(TermStore t)
-	{
-		
-	}
+
+    LinkedList<Term> expression; 
+    ListIterator<Term> cursor;
+
+    public Polynomial(){
+        expression = new LinkedList<Term>();
+        cursor = expression.listIterator();
+    }
+
+    public void sort(){
+        cursor = expression.listIterator();
+
+        Term prev=cursor.next();
+        Term next;
+        while(cursor.hasNext()){
+            if(cursor.next().getY() > prev.getY()){
+                next=cursor.previous();
+                cursor.remove();
+                cursor.previous();
+                cursor.add(next);
+                sort();
+            }
+            continue;
+        }
+    }
+
+    public void add(Term thing){
+        if(expression.size() >= 1)
+            sort();
+
+
+        boolean inserted = false;
+        cursor = expression.listIterator();
+        Term existing;
+        while(cursor.hasNext()){
+            existing = cursor.next();
+            //if something with same exponent already exists, just add coefficient
+            if(existing.getY() == thing.getY()){
+                existing.setX(existing.getX()+thing.getX());
+                cursor.remove();
+                cursor.add(existing);
+                inserted = true;
+                return;
+            }
+
+            //if past that exponent value, backtrack and add before. (assumes in order of highest to lowest exponents from left to right)
+            if(existing.getY() < thing.getY()){
+                cursor.previous();
+                cursor.add(thing);
+                inserted = true;
+                return;
+            }
+        }
+        if(!inserted)
+            cursor.add(thing);
+    }
+
+    public void add(Polynomial thing){
+        cursor = thing.getList().listIterator();
+        while(cursor.hasNext()){
+            expression.add(cursor.next());
+        }
+        sort();
+    }
+
+    public void multiply(Term thing){
+        cursor = expression.listIterator();
+        Term current;
+        while(cursor.hasNext()){
+            current = cursor.next();
+            current.setX(current.getX() * thing.getX());
+            current.setY(current.getY() + thing.getY());
+            cursor.previous();
+            cursor.remove();
+            cursor.add(current);
+        }
+    }
+
+    public LinkedList<Term> multiplyTerm(Term thing, LinkedList<Term> newThing){
+        ListIterator<Term> newC = newThing.listIterator();
+        Term current;
+        while(newC.hasNext()){
+            current = newC.next();
+            current = new Term(current.getX() * thing.getX(), current.getY() + thing.getY());
+            newC.remove();
+            newC.add(current);
+        }
+        return newThing;
+    }
+
+    public void multiply(Polynomial p){
+        ListIterator<Term> pIter = p.getList().listIterator();
+        cursor = expression.listIterator();
+
+        LinkedList<Term> newExp = new LinkedList<Term>();
+        while(cursor.hasNext()){
+            newExp.addLast(cursor.next());
+        }
+
+        cursor = expression.listIterator();
+
+        LinkedList<Term> toAdd = new LinkedList<Term>();
+        
+        while(pIter.hasNext()){
+            toAdd.addAll(multiplyTerm(pIter.next(), newExp));
+            
+            //reset copy of expression
+            cursor = expression.listIterator();
+            newExp = new LinkedList<Term>();
+            while(cursor.hasNext()){
+                newExp.addLast(cursor.next());
+            }
+        }
+
+        expression = toAdd;
+        sort();
+    }
+
+
+    public LinkedList<Term> derivative(){
+        LinkedList<Term> ret = new LinkedList<Term>();
+
+        cursor = expression.listIterator();
+        Term current;
+        while(cursor.hasNext()){
+            current = cursor.next();
+            if(current.getY() != 0){
+                ret.add(new Term(current.getY()*current.getX(), current.getY()-1));
+            }
+
+        }
+        return ret;
+    }
+
+    public LinkedList<Term> getList(){
+        return expression;
+    }
+
+    public String toString(){
+        cursor = expression.listIterator();
+        Term thing = cursor.next();
+        String retString = thing.toString();
+
+        while(cursor.hasNext()){
+            thing = cursor.next();
+            retString += " + "+thing.toString();
+        }
+
+        return retString;
+    }
+
 }
