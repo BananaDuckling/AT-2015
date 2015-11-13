937ccc51d4b8870f001c2882efc6bf54500a18f2
16jc0080 <16jc0080@HSS-101-10.westwindsor.k12.nj.us>
2015-11-13 13:43:40 -0500

    lel

--- LinkedLists/src/histoNodes/HistoList.java
+++ LinkedLists/src/histoNodes/HistoList.java
@@ -27,67 +27,66 @@ public class HistoList
 	public void addLetter(char l)
 	{
 		if(front == null)
-		{
-			front = new HistoNode(l, 1, null);
-		}
-		else
-		{
-			HistoNode h = front;
-			while(front.getNext()  != null)
-			{
-				h = front.getNext();
-				if(h.getLetter() == l)
-					h.addLetter();
+			front = new HistoNode(l, 0, null);
+		HistoNode temp = front;
+		while(temp.getNext() != null){
+			if(temp.getLetter() == l){
+				temp.setLetterCount(temp.getLetterCount() + 1);
+				return;
 			}
+			else
+				temp = temp.getNext();
 		}
+		if(temp.getLetter() == l)
+			temp.setLetterCount(temp.getLetterCount() + 1);
+		else
+		temp.setNext(new HistoNode(l, 1, null));
 	}
 
 	//returns the index position of let in the list if let exists
 	public int indexOf(char let)
 	{
-
-
-
-
-
-
-
-
-
+		int pos = 0;
+		HistoNode temp = front;
+		while(temp != null)
+		{
+			if(temp.getLetter() == let)
+				return pos;
+			temp = temp.getNext();
+			pos++;
+		}
 		return -1;
 	}
 
 	//returns a reference to the node at spot
 	private HistoNode nodeAt(int spot)
 	{
-		HistoNode current=null;
-
-
-
-
-
-
-
-
-
-
-		return current;
+		int i = 0;
+		HistoNode temp = front;
+		
+		if(spot == 0)
+			return front;
+		
+		while(temp.getNext() != null)
+		{
+			if(spot == i)
+				return temp;
+			temp = temp.getNext();
+			i++;
+		}
 	}
 
 	//returns a string will all values from list
 	public String toString()
 	{
 		String output = "";
-
-
-
-
-
-
-
-
-
-
+		HistoNode temp = front;
+		
+		while(temp !=  null)
+		{
+			output += temp.getLetter()+": "+temp.getLetterCount()+"\t";
+			temp = temp.getNext();
+		}
 		return output;
 	}
 }
