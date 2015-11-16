a6d608455bd0a68b9d1a485cc2b562e95ff3c7a2
16jc0080 <16jc0080@HSS-101-10.westwindsor.k12.nj.us>
2015-11-16 14:48:41 -0500

    lmaoo

---
+++ LinkedLists/src/worksheet5_1/Linkable.java
@@ -0,0 +1,14 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -  
+//Date -
+//Class - 
+//Lab  -
+package worksheet5_1;
+
+public interface Linkable
+{
+   Comparable getValue();
+   Linkable getNext();
+   void setNext(Linkable next);
+   void setValue(Comparable value);
+}
---
+++ LinkedLists/src/worksheet5_2/Linkable.java
@@ -0,0 +1,14 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -  
+//Date -
+//Class - 
+//Lab  -
+package worksheet5_2;
+
+public interface Linkable
+{
+   Comparable getValue();
+   Linkable getNext();
+   void setNext(Linkable next);
+   void setValue(Comparable value);
+}
