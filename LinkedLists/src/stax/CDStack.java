185958937ff990c43387fa66560506863b087aff
16jc0080 <16jc0080@HSS-101-10.westwindsor.k12.nj.us>
2015-11-30 14:44:32 -0500

    lol

--- LinkedLists/src/stax/CDStack.java
+++ LinkedLists/src/stax/CDStack.java
@@ -6,6 +6,10 @@ public class CDStack {
 	
 	public CDStack()
 	{
+		initialize();
+	}
+	private void initialize()
+	{
 		myStack = new Stack<String>();
 		myStack.push("Journey");
 		myStack.push("Iggy pop");
@@ -28,16 +32,55 @@ public class CDStack {
 	{
 		return myStack.pop();
 	}
-	public void printCd()
+	public void printCdManual()
 	{
+		//method 1
 		while(!myStack.empty())
 		{
 			System.out.println(myStack.pop() + " ");
 		}
 	}
-	
-	public static void main(String[] args)
+	public void printCdIterator()
 	{
-		
+		initialize();
+				Iterator<String> i = myStack.iterator();
+				while(i.hasNext())
+					System.out.println(i.next());
 	}
+	
+	public void printCdForEach()
+	{
+		initialize();
+				for(String s : myStack)
+					System.out.println(s);
+	}
+	
+	public void printCdRecursive()
+	{
+		initialize();
+		recur();		
+	}
+	public void recur()
+	{
+		if(myStack.empty())
+			return;
+		else
+		{
+			System.out.println(myStack.pop());
+			recur();
+		}
+	}
+	
+	public void reverseStack()
+	{
+		initialize();
+		System.out.println(myStack);
+		Stack<String> temp = new Stack();
+		while(!myStack.empty())
+		{
+			temp.push(myStack.pop());
+		}
+		System.out.println(temp);
+	}
+	
 }
---
+++ LinkedLists/src/stax/StaxTester.java
@@ -0,0 +1,21 @@
+package stax;
+
+import java.util.Iterator;
+
+public class StaxTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CDStack cd = new CDStack();
+//		System.out.println("manual print");
+//		cd.printCdManual();
+//		System.out.println("\niterator print");
+//		cd.printCdIterator();
+//		System.out.println("\nforEach print");
+//		cd.printCdForEach();
+//		System.out.println("\nrecursive print");
+//		cd.printCdRecursive();
+		cd.reverseStack();
+	}
+
+}
