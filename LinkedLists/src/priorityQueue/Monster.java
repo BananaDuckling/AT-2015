5adce7de504caff8d6a557b50a1f45afa120320c
16jc0080 <16jc0080@HSS-101-11.westwindsor.k12.nj.us>
2016-01-08 12:39:11 -0500

    lol

---
+++ LinkedLists/src/priorityQueue/Monster.java
@@ -0,0 +1,78 @@
+package priorityQueue;
+
+public class Monster implements Comparable<Monster> {
+	private int height, weight, age;
+	
+	public Monster()
+	{
+		
+	}
+	
+	public Monster(int h, int w, int a)
+	{
+		height = h;
+		weight = w;
+		age = a;
+	}
+	
+	public Object clone()
+	{
+		return new Monster(height, weight, age);
+	}
+	
+	public boolean equals(Object o)
+	{
+		Monster m = (Monster)o;
+		return(m.getHeight() == this.height && 
+				m.getWeight() == this.weight &&
+				m.getAge() == this.age);
+	}
+	
+	public int compareTo(Monster m)
+	{
+		if(this.height > m.getHeight())
+			return 1;
+		else if(this.height == m.getHeight())
+		{
+			if(this.weight > m.getWeight()	)
+				return 1;
+			else if(this.weight == m.getWeight())
+			{
+				if(this.age > m.getAge())
+					return 1;
+				else if(this.age == m.getAge())
+					return 0;
+				else
+					return -1;
+			}
+			else 
+				return -1;
+		}
+		else
+			return -1;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
--- LinkedLists/src/priorityQueue/PQTestRunner.java
+++ LinkedLists/src/priorityQueue/PQTestRunner.java
@@ -3,11 +3,20 @@
 //Date -
 //Class -
 //Lab  -  
+package priorityQueue;
+import java.util.PriorityQueue;
 
-public class PQTestRunner
-{
+public class PQTestRunner {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		PriorityQueue<Integer> pq;
+		pq = new PriorityQueue<Integer>();
+		pq.add(67);
+		pq.add(34);
+		pq.add(12);
+			
+		System.out.println(pq.remove()); //outs 12
+		System.out.println(pq.remove()); //outs 34
+		System.out.println(pq.remove()); //outs 67	
 	}
 }
