ca1a532b9ddb506ba3fac9b2f0dd2d1420374ef9
Justin <justinchao20@gmail.com>
2016-02-23 01:13:01 -0500

    top kekkerinos. Test th is plzz.

--- LinkedLists/src/hashTables/PhoneBook.java
+++ LinkedLists/src/hashTables/PhoneBook.java
@@ -1,5 +1,6 @@
 package hashTables;
 import java.util.*;
+import java.io.*;
 public class PhoneBook {
 	
 	private int numSlots;
@@ -34,7 +35,8 @@ public class PhoneBook {
 	{
 		for(Integer i :h.keySet())
 		{
-			System.out.println("Last name first letter:"+h.get);
+			int temp = i;
+			System.out.println("Last name first letter:"+(char)temp);
 		}
 	}
 	
@@ -45,32 +47,76 @@ public class PhoneBook {
 	
 	public int getSize()
 	{
-		
+		int ret = 0;
+		for(int i : h.keySet())
+		{
+			for(PhoneEntry p : h.get(i))
+				ret++;
+		}
+		return ret;
 	}
 	
-	public void load()
+	public void load(File f) throws FileNotFoundException
 	{
-		
+		Scanner k = new Scanner(f);
+		while(k.hasNextLine())
+		{
+			Scanner k1 = new Scanner(k.nextLine());
+			PhoneEntry entry = new PhoneEntry(k.next(), Integer.parseInt(k.next()));
+		}
 	}
 	
 	public int getNumberNulls()
 	{
-		
+		int numNulls = 0;
+		for(int i : h.keySet())
+		{
+			if(h.get(i) == null)
+				numNulls ++;
+		}
+		return numNulls;
 	}
 	
 	public int getLongestList()
 	{
-		
+		int longestSize = 0;
+		for(int i : h.keySet())
+		{
+			if(h.get(i).size() > longestSize)
+				longestSize = h.get(i).size();
+		}
+		return longestSize;
 	}
 	
 	public String lookup(String lookFor)
 	{
-		
+		for(int i : h.keySet())
+		{
+			for(PhoneEntry e : h.get(i))
+			{
+				if(e.getName().equalsIgnoreCase(lookFor))
+					return e.getNumber()+"";
+			}
+				
+		}
+		return "not found";
 	}
 	
 	public boolean changeNumber(String lookFor, String newNum)
 	{
-		
+		for(int i : h.keySet())
+		{
+			for(PhoneEntry e : h.get(i))
+			{
+				if(e.getName().equalsIgnoreCase(lookFor))
+				{
+					e.setNumber(Integer.parseInt(newNum));
+					return true;
+				}
+			}
+				
+		}
+		return false;
 	}
 }
 
