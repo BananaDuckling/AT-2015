7c42890d812a6bdd02e619ae9c2213f5799f0735
16jc0080 <16jc0080@HSS-101-11.westwindsor.k12.nj.us>
2016-05-06 13:43:38 -0400

    sdfsdfsdfsdfsdfsd

--- LinkedLists/src/graphs/ShortestPathGraph.java
+++ LinkedLists/src/graphs/ShortestPathGraph.java
@@ -4,82 +4,82 @@
 //Class -
 //Lab  -
 package graphs;
-import java.io.File;
-import java.io.IOException;
+
 import java.util.*;
 
 import static java.lang.System.*;
 
-public class ShortestPathGraph
-{
-	private TreeMap<String, Set<String>> map;
+public class ShortestPathGraph {
+	private TreeMap<String, HashSet<String>> map;
 	private boolean yesOrNo = false;
-	private int shortest = 1;
+	private int shortest = 0;
 
-	public ShortestPathGraph(String line)
+	public ShortestPathGraph(String line) 
 	{
-		String[] input = line.split(" ");
-		map = new TreeMap<String, Set<String>>();
-		for(String s : input)
-		{
-			String firstLetter = s.substring(0,1);
-			String secondLetter = s.substring(1,2);
-			
-			if(!map.containsKey(firstLetter))
-			{
-				Set<String> newSet = new TreeSet<String>();
-				newSet.add(secondLetter);
-				map.put(firstLetter,newSet);
-			}
-			else
-				map.get(firstLetter).add(secondLetter);
-			if(!map.containsKey(secondLetter))
-			{
-				Set<String> newSet = new TreeSet<String>();
-				newSet.add(firstLetter);
-				map.put(secondLetter,newSet);
-			}
-			else
-				map.get(secondLetter).add(firstLetter);
-		}
-	}
-
-	public boolean contains(String letter)
-	{
-		return map.containsKey(letter);
-	}
-
-	public void check(String first, String second, ArrayList<String> been, int steps)
-	{
-		if (!yesOrNo) 
-		{
-			if (map.get(first).contains(second))
-			{
-				yesOrNo = true;
-				if(steps < shortest)
-					shortest = steps;
-			}
+		map = new TreeMap<String, HashSet<String>>();
+		String[] inputString = line.split(" ");
+		
+		for (int x = 1; x < inputString.length; x++) {
+			if (map.containsKey(inputString[x].substring(0, 1))) {
+				HashSet<String> t = map.get(inputString[x].substring(0, 1));
+				t.add(inputString[x].substring(1));
+				map.put(inputString[x].substring(0, 1), t);
+			} 
 			else 
 			{
-				Set<String> set = map.get(first);
-				for (String s : set) 
-				{
-					boolean haveToAdd = false;
-					if (!been.contains(s)) 
-					{
-						haveToAdd = true;
-						been.add(s);
-						check(s, second, been, steps +1);
-					}
-					if(haveToAdd)
-						set.remove(s);
-				}
+				HashSet<String> t = new HashSet<String>();
+				t.add(inputString[x].substring(1));
+				map.put(inputString[x].substring(0, 1), t);
+			}
+
+			if (map.containsKey(inputString[x].substring(1))) {
+				HashSet<String> t = map.get(inputString[x].substring(1));
+				t.add(inputString[x].substring(0, 1));
+				map.put(inputString[x].substring(1), t);
+			} 
+			else 
+			{
+				HashSet<String> t = new HashSet<String>();
+				t.add(inputString[x].substring(0, 1));
+				map.put(inputString[x].substring(1), t);
 			}
 		}
 	}
 
-	public String toString()
+	public boolean contains(String letter) {
+		return map.containsKey(letter);
+	}
+	
+	public boolean match()
 	{
-		return "";
+		return yesOrNo;
+	}
+	public int shortest()
+	{
+		return shortest;
+	}
+
+	public void check(String first, String second, ArrayList<String> been, int steps) {
+			if (map.get(first).contains(second)) 
+			{
+				yesOrNo = true;
+				if (shortest == 0 || steps <= shortest)
+					shortest = steps;
+			} 
+			else 
+			{
+				HashSet<String> set = map.get(first);
+				for (String x : set){
+					//this is here so we can perform calcs without affecting recursion
+					boolean haveToReadd = false;
+					if (!been.contains(x)) {
+						haveToReadd = true;
+						been.add(x);
+						check(x, second, been, steps + 1);
+					}
+					if(haveToReadd)
+						been.remove(x);
+				}
+			}
 	}
 }
--- LinkedLists/src/graphs/ShortestPathGraphRunner.java
+++ LinkedLists/src/graphs/ShortestPathGraphRunner.java
@@ -1,17 +1,30 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -  
-//Class -
-//Lab  -
+package graphs;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+
 import static java.lang.System.*;
 
-public class ShortestPathGraphRunner
-{
-	public static void main( String[] args ) throws IOException
-	{
+public class ShortestPathGraphRunner {
+	public static void main(String[] args) throws IOException {
+		Scanner file = new Scanner(new File("H:\\git\\AT-2015\\LinkedLists\\src\\graphs\\graph1.dat"));
+		int howManyTimes = Integer.parseInt(file.nextLine());
+		for (int x = 0; x < howManyTimes; x++) 
+		{
+			ShortestPathGraph g = new ShortestPathGraph(" "+file.nextLine());
+			ArrayList<String> arr = new ArrayList<String>();
+			String check = file.nextLine();
+			
+			arr.add(check.substring(0, 1));
+			g.check(check.substring(0, 1), check.substring(1), arr, 1);
+			
+			if(g.match())
+				System.out.println("["+check.substring(0, 1) + "] connects to "
+					+ "["+check.substring(1) + "] in " + (g.shortest()-1) + " steps");
+			else
+				System.out.println("["+check.substring(0, 1)+"] does not connect to "+
+			check.substring(1));
+		}
 	}
 }
